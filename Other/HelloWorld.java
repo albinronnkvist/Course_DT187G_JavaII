b3bce61a5c2aa6d506bdfa73e7a4b77bb6eb26de
Albin <albinronnkvist@protonmail.com>
2024-09-01 19:13:45 +0200

    Create Simple HelloWorld console app

---
+++ Other/HelloWorld.java
@@ -0,0 +1,9 @@
+package Other;
+
+// Compile: javac Other/HelloWorld.java
+// Execute: java Other/HelloWorld
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World! " + args[0]);
+    }
+}
