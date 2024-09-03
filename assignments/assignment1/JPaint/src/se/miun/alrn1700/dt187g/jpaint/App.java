d2efa216ccd285763210f7ecf835e562527af5c4
Albin <albinronnkvist@protonmail.com>
2024-09-03 18:51:37 +0200

    Create JPaint project

---
+++ assignments/assignment1/JPaint/src/se/miun/alrn1700/dt187g/jpaint/App.java
@@ -0,0 +1,13 @@
+/**
+* A short description (in Swedish or English) of the class.
+*
+* @author Albin Rönnkvist (alrn1700)
+* @version 1.0
+*/
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
---
+++ experiments/jpaint/src/main/java/se/miun/alrn1700/dt187g/jpaint/Main.java
@@ -0,0 +1,7 @@
+package se.miun.alrn1700.dt187g.jpaint;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
