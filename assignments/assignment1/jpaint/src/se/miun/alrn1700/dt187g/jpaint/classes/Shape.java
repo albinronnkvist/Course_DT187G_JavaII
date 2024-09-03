5d7ed0d3d84a29a46bbaa30516a4faa03ede6d65
Albin <albinronnkvist@protonmail.com>
2024-09-03 19:45:55 +0200

    Add Shape class

---
+++ assignments/assignment1/jpaint/src/se/miun/alrn1700/dt187g/jpaint/classes/Shape.java
@@ -0,0 +1,41 @@
+package se.miun.alrn1700.dt187g.jpaint;
+/**
+* TODO: explain
+*
+* @author Albin Rönnkvist (alrn1700)
+* @version 1.0
+*/
+
+public abstract class Shape implements Drawable {
+    private String color;
+    private Point[] points;
+
+    public Shape(Point p, String color) {
+        this.color = color;
+        this.points = new Point[1];
+        this.points[0] = p;
+    }
+
+    public getColor() {
+        return this.color;
+    }
+
+    public setColor(String color) {
+        this.color = color;
+    }
+
+    public abstract double getCircumference() {
+    }
+
+    public abstract double getArea() {
+    }
+
+    public abstract void addPoint(Point p) {
+    }
+
+    public abstract void addPoint(double x, double y) {
+    }
+
+    public abstract boolean hasEndpoint() {
+    }
+}
