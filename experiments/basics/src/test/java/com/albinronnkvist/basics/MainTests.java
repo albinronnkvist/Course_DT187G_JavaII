d427bc6b25514d3785e91d6a05d52e6f1f8c6012
Albin <albinronnkvist@protonmail.com>
2024-09-04 14:08:20 +0200

    Setup tests

---
+++ experiments/basics/src/test/java/com/albinronnkvist/basics/MainTests.java
@@ -0,0 +1,12 @@
+package com.albinronnkvist.basics;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class MainTests {
+    @Test
+    public void test() {
+        assertThat(1).isEqualTo(1);
+    }
+}
