3dd2e7755ed20db57e9dba9c89f6edb47ef26f00
Albin <albinronnkvist@protonmail.com>
2024-09-04 17:44:14 +0200

    Add Enum dataype

---
+++ experiments/basics/src/main/java/com/albinronnkvist/basics/datatypes/EnumType.java
@@ -0,0 +1,14 @@
+package com.albinronnkvist.basics.datatypes;
+
+/*
+An enum is a special class that represents a group of constants.
+*/
+public enum EnumType {
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY,
+    SUNDAY
+}
