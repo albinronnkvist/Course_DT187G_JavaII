27a643e97147cb8f979263b6e85826c49a5f7f81
Albin <albinronnkvist@protonmail.com>
2024-09-05 18:16:07 +0200

    Add description to Drawable

--- assignments/assignment1/jpaint/src/se/miun/alrn1700/dt187g/jpaint/interfaces/Drawable.java
+++ assignments/assignment1/jpaint/src/se/miun/alrn1700/dt187g/jpaint/interfaces/Drawable.java
@@ -1,6 +1,9 @@
 package se.miun.alrn1700.dt187g.jpaint.interfaces;
 /**
-* TODO: explain
+* An interface that other "drawable" classes can implement.
+* The interface defines a set of public abstract methods (methods without implementation).
+*   - It uses method overloading which allows multiple methods in the same class to share the same name but with different parameters.
+*   - Classes that implement this interface must provide implementations for these methods.
 *
 * @author Albin Rönnkvist (alrn1700)
 * @version 1.0
