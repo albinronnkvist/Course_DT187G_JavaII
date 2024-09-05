8f3fa73e03bf50ff85e85936c80d556b470997df
Albin <albinronnkvist@protonmail.com>
2024-09-05 22:33:03 +0200

    Implement Circle

--- assignments/assignment1/jpaint/src/se/miun/alrn1700/dt187g/jpaint/classes/shapes/Circle.java
+++ assignments/assignment1/jpaint/src/se/miun/alrn1700/dt187g/jpaint/classes/shapes/Circle.java
@@ -8,7 +8,6 @@
 package se.miun.alrn1700.dt187g.jpaint.classes.shapes;
 
 import java.awt.Graphics;
-
 import se.miun.alrn1700.dt187g.jpaint.classes.Point;
 import se.miun.alrn1700.dt187g.jpaint.classes.Shape;
 
@@ -23,50 +22,73 @@ public class Circle extends Shape {
         this(new Point(x, y), color);
     }
 
-    // TODO: implement
     public double getRadius() {
-        return 0;
+        if(!hasEndpoint()){
+            return 0;
+        }
+        
+        var startPointX = points[0].getX();
+        var startPointY = points[0].getY();
+        var endPointX = points[1].getX();
+        var endPointY = points[1].getY();
+        
+        return Math.sqrt(Math.pow(endPointX - startPointX, 2) + Math.pow(endPointY - startPointY, 2));
     }
 
+    @Override
+    public double getCircumference() {
+        if(!hasEndpoint()){
+            return 0;
+        }
+
+        return 2 * PI * getRadius();
+    }
+    
+    @Override
+    public double getArea() {
+        if(!hasEndpoint()){
+            return 0;
+        }
+
+        return PI * Math.pow(getRadius(), 2);
+    }
 
     @Override
     public void draw() {
-        throw new UnsupportedOperationException("Unimplemented method 'draw'");
+        System.out.println(this.toString());
     }
 
+    // Implement in another assignment
     @Override
     public void draw(Graphics g) {
         throw new UnsupportedOperationException("Unimplemented method 'draw'");
     }
 
     @Override
-    public double getCircumference() {
-        throw new UnsupportedOperationException("Unimplemented method 'getCircumference'");
-    }
-
-    @Override
-    public double getArea() {
-        throw new UnsupportedOperationException("Unimplemented method 'getArea'");
-    }
-
-    @Override
     public void addPoint(Point p) {
-        throw new UnsupportedOperationException("Unimplemented method 'addPoint'");
+        points[1] = p;
     }
 
     @Override
     public void addPoint(double x, double y) {
-        throw new UnsupportedOperationException("Unimplemented method 'addPoint'");
+        addPoint(new Point(x, y));
     }
 
     @Override
     public boolean hasEndpoint() {
-        throw new UnsupportedOperationException("Unimplemented method 'hasEndpoint'");
+        return points[1] != null;
     }
 
-    // TODO: implement
     @Override
     public String toString() {
-        throw new UnsupportedOperationException("Unimplemented method 'toString'");
+        var rectangleInfo = new StringBuilder();
+        rectangleInfo.append("Circle[");
+        rectangleInfo.append("start=" + points[0].toString()); 
+        rectangleInfo.append(" end=" + (hasEndpoint() ? points[1].toString() : "N/A"));
+        rectangleInfo.append(" radius=" + (hasEndpoint() ? getRadius() : "N/A"));
+        rectangleInfo.append(" color=" + (getColor() != null && !getColor().trim().isEmpty() ? getColor() : "N/A"));
+        rectangleInfo.append("]");
+
+        return rectangleInfo.toString();
     }
 }
