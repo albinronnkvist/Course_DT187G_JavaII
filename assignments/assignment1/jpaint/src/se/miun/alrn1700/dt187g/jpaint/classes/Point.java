bed7f47eccfbf7ae0a28d8027d926256cf859737
Albin <albinronnkvist@protonmail.com>
2024-09-05 18:34:42 +0200

    Add description to Point

--- assignments/assignment1/jpaint/src/se/miun/alrn1700/dt187g/jpaint/classes/Point.java
+++ assignments/assignment1/jpaint/src/se/miun/alrn1700/dt187g/jpaint/classes/Point.java
@@ -1,6 +1,18 @@
 package se.miun.alrn1700.dt187g.jpaint.classes;
 /**
-* TODO: explain
+* A class which represents a Point in 2D space with x and y coordinates.
+*
+* The class uses encapsulation to hide the implementation details of the x and y coordinates.
+*   - The instance variables are private and cannot be accessed from outside the class.
+*   - It provides getter and setter methods to access and modify the private x and y coordinates.
+*
+* It has two constructors:
+*   - A default constructor which sets the x and y coordinates to 0.
+*     This is necessary since we have another explicitly defined constructor, and a default constructor is not provided by the compiler in that case.
+*   - A parameterized constructor which takes two arguments: x and y coordinates.
+*
+* It also overrides the toString() method to return a custom string representation of the point.
+*   - All classes inherits from the Object class, which is where the default toString() method is defined.
 *
 * @author Albin Rönnkvist (alrn1700)
 * @version 1.0
