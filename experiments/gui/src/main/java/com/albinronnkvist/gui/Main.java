3589f91175a0004e8df5e693949757fc36fdecfe
Albin <albinronnkvist@protonmail.com>
2024-09-20 12:19:36 +0200

    Add CustomMenuBarFrame

--- experiments/gui/src/main/java/com/albinronnkvist/gui/Main.java
+++ experiments/gui/src/main/java/com/albinronnkvist/gui/Main.java
@@ -1,9 +1,9 @@
 package com.albinronnkvist.gui;
 
-import com.albinronnkvist.gui.components.CustomSliderFrame;
+import com.albinronnkvist.gui.components.CustomMenuBarFrame;
 
 public class Main {
     public static void main(String[] args) {
-        var frame = new CustomSliderFrame();
+        var frame = new CustomMenuBarFrame();
     }
 }
---
+++ experiments/gui/src/main/java/com/albinronnkvist/gui/components/CustomMenuBarFrame.java
@@ -0,0 +1,47 @@
+package com.albinronnkvist.gui.components;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class CustomMenuBarFrame extends CustomFrame implements ActionListener {
+    private JMenuBar menuBar;
+    private JMenu fileMenu;
+    private JMenuItem openItem, saveItem, exitItem;
+
+    public CustomMenuBarFrame() {
+        menuBar = new JMenuBar();
+
+        fileMenu = new JMenu("File");
+
+        openItem = new JMenuItem("Open");
+        saveItem = new JMenuItem("Save");
+        exitItem = new JMenuItem("Exit");
+
+        openItem.addActionListener(this);
+        saveItem.addActionListener(this);
+        exitItem.addActionListener(this);
+
+        fileMenu.add(openItem);
+        fileMenu.add(saveItem);
+        fileMenu.addSeparator();
+        fileMenu.add(exitItem);
+
+        menuBar.add(fileMenu);
+        
+        this.setJMenuBar(menuBar);
+        this.setSize(400, 300);
+        this.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == openItem) {
+            JOptionPane.showMessageDialog(this, "Open clicked");
+        } else if (e.getSource() == saveItem) {
+            JOptionPane.showMessageDialog(this, "Save clicked");
+        } else if (e.getSource() == exitItem) {
+            System.exit(0);
+        }
+    }
+}
