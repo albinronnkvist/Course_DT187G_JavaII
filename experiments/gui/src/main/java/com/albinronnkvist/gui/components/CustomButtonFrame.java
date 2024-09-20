ae263c3db99e02901a0e954c2636ed2217fe12f3
Albin <albinronnkvist@protonmail.com>
2024-09-20 11:00:27 +0200

    Add button icon

--- experiments/gui/src/main/java/com/albinronnkvist/gui/components/CustomButtonFrame.java
+++ experiments/gui/src/main/java/com/albinronnkvist/gui/components/CustomButtonFrame.java
@@ -1,5 +1,6 @@
 package com.albinronnkvist.gui.components;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 
 public class CustomButtonFrame extends CustomFrame { // implements ActionListener
@@ -7,9 +8,15 @@ public class CustomButtonFrame extends CustomFrame { // implements ActionListene
     private JButton button;
 
     public CustomButtonFrame() {
-        this.button = new JButton();
-        this.button.setText("Click me!");
-        this.button.setBounds(200, 100, 100, 50);
+        var icon = new ImageIcon(getClass().getResource("/logo.png"));
+        button = new JButton();
+        button.setText("Click me!");
+        button.setBounds(200, 100, 200, 50);
+        button.setFocusable(false);
+        button.setIcon(icon);
+        button.setHorizontalTextPosition(JButton.RIGHT);
+        button.setVerticalTextPosition(JButton.CENTER);
+
         button.addActionListener(e -> System.out.println("You clicked the button!")); // Shortcut
 
         this.add(button);
