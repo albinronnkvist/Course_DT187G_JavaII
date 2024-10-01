36c37e5a1dd53f1eccf605da2d2d7a02826fd85d
Albin <albinronnkvist@protonmail.com>
2024-10-01 17:33:06 +0200

    Implement missing parts in DrawingPanel

--- assignments/assignment4/jpaint/src/se/miun/alrn1700/dt187g/jpaint/gui/DrawingPanel.java
+++ assignments/assignment4/jpaint/src/se/miun/alrn1700/dt187g/jpaint/gui/DrawingPanel.java
@@ -4,15 +4,11 @@ import java.awt.Color;
 
 import javax.swing.JPanel;
 
-public class DrawingPanel extends JPanel {
-	
+import se.miun.alrn1700.dt187g.jpaint.Drawing;
 
-	/*
-	 * Beroende på vilken konstruktor som anropas så kommer antingen 
-	 * bakgrunden för denna komponent sättas till den färg som skickas som argument,
-	 * alternativt sätts bakgrunden till vit (Color.WHITE).
-	 */
-	
+public class DrawingPanel extends JPanel {
+	private Drawing drawing;
+
 	public DrawingPanel() {
 		this(Color.WHITE);
 	}
@@ -21,4 +17,11 @@ public class DrawingPanel extends JPanel {
 		this.setBackground(backgroundColor);
 	}
 	
+	public Drawing getDrawing() {
+		return drawing;
+	}
+
+	public void setDrawing(Drawing drawing) {
+		this.drawing = drawing;
+	}
 }
