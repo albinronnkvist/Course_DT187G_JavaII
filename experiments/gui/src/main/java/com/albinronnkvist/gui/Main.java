2bf98953323dc9504b62259920961d525910ab5b
Albin <albinronnkvist@protonmail.com>
2024-10-17 21:08:03 +0200

    Add JFileChooser examples

--- experiments/gui/src/main/java/com/albinronnkvist/gui/Main.java
+++ experiments/gui/src/main/java/com/albinronnkvist/gui/Main.java
@@ -1,9 +1,9 @@
 package com.albinronnkvist.gui;
 
-import com.albinronnkvist.gui.graphics.GraphicsFrame;
+import com.albinronnkvist.gui.components.CustomFileChooserFrame;
 
 public class Main {
     public static void main(String[] args) {
-        var frame = new GraphicsFrame();
+        var frame = new CustomFileChooserFrame();
     }
 }
--- experiments/gui/src/main/java/com/albinronnkvist/gui/components/CustomFileChooserFrame.java
+++ experiments/gui/src/main/java/com/albinronnkvist/gui/components/CustomFileChooserFrame.java
@@ -1,35 +1,56 @@
 package com.albinronnkvist.gui.components;
 
-import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
-public class CustomFileChooserFrame extends CustomFrame implements ActionListener {
-    private JButton button;
+import java.awt.FlowLayout;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class CustomFileChooserFrame extends CustomFrame {
+    private FileNameExtensionFilter txtFilter = new FileNameExtensionFilter("Text Files", "txt");
 
     public CustomFileChooserFrame() {
-        button = new JButton("Select File");
-        button.addActionListener(this);
+        this.setLayout(new FlowLayout());
 
-        var panel = new JPanel();
-        panel.add(button);
+        var openButton = new JButton("Choose File");
+        openButton.addActionListener(e -> HandleOpen());
 
-        this.add(panel);
+        var saveButton = new JButton("Save File");
+        saveButton.addActionListener(e -> HandleSave());
+
+        this.add(openButton);
+        this.add(saveButton);
         this.setVisible(true);
     }
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == button) {
-            var fileChooser = new JFileChooser();
-            
-            int result = fileChooser.showOpenDialog(this);
-            
-            if (result == JFileChooser.APPROVE_OPTION) {
-                var selectedFile = fileChooser.getSelectedFile();
-                JOptionPane.showMessageDialog(this, "Selected file: " + selectedFile.getAbsolutePath());
-            } else {
-                JOptionPane.showMessageDialog(this, "File selection canceled.");
+    public void HandleOpen() {
+        var fileChooser = new JFileChooser();
+
+        var option = fileChooser.showOpenDialog(this);
+
+        if (option == JFileChooser.APPROVE_OPTION) {
+            var file = fileChooser.getSelectedFile();
+            System.out.println("Selected file to open: " + file.getAbsolutePath());
+        }
+    }
+
+    public void HandleSave() {
+        var fileChooser = new JFileChooser();
+        fileChooser.setAcceptAllFileFilterUsed(false);
+        fileChooser.setFileFilter(txtFilter);
+        
+        var option = fileChooser.showSaveDialog(this);
+
+        if (option == JFileChooser.APPROVE_OPTION) {
+            var file = fileChooser.getSelectedFile();
+
+            try (FileWriter writer = new FileWriter(file)) {
+                writer.write("Hi Hello How Are You...");
+                System.out.println("File saved: " + file.getAbsolutePath());
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         }
     }
